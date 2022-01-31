01ffbf4a7180835c9fa318eb129f11e98202d5d2
anthonycastellano <=>
2022-01-30 19:39:45 -0500

    changed app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello again docker community!
+          Hello again docker community! Changed again
         </p>
         <a
           className="App-link"
